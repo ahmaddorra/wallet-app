1433fd019b30f918f70bb6b243402e47e8c49dd5
Ahmad Dorra <ahmadeldorra1@gmail.com>
2022-07-24 18:37:23 +0300

    Initial Commit

---
+++ wallet/walletApp.swift
@@ -0,0 +1,17 @@
+//
+//  walletApp.swift
+//  wallet
+//
+//  Created by nour on 24/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct walletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
